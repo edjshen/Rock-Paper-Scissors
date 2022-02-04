161f01f3555edce73aaabc70c415a1045303ea70
edjshen <75459293+edjshen@users.noreply.github.com>
2022-02-04 13:15:49 -0500

    update to extra credit 2

--- game_test.py
+++ game_test.py
@@ -8,16 +8,20 @@ Created on Wed Feb  2 13:10:41 2022
 from Rock_Paper_Scissors import validate
 
 def test_determination_of_the_winner():
-    assert validate(0, 0) == 0 # represents a tie
+    assert validate(0, 0) == 0 # represents a tie      
     assert validate(0, 1) == 2
     assert validate(0, 2) == 1
 
     assert validate(1, 0) == 1
-    assert validate(1, 1) == 0# represents a tie
+    assert validate(1, 1) == 0 # represents a tie
     assert validate(1, 2) == 2
 
     assert validate(2, 0) == 2
     assert validate(2, 1) == 1
     assert validate(2, 2) == 0 # represents a tie
+    
+def main():
+    test_determination_of_the_winner()
+    main()
     
     
